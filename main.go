006e0f89207447deae1280d546a283373543ece5
baolhq <baolhq280@gmail.com>
2024-12-02 21:55:23 +0700

    organize project, boilerplates

--- main.go
+++ main.go
@@ -1,29 +1,7 @@
 package main
 
-import (
-	"baolhq/gotem/lib"
-	"log"
-
-	"github.com/spf13/cobra"
-)
+import "baolhq/gotem/cmd"
 
 func main() {
-	rootCmd := &cobra.Command{
-		Use:   "gotem",
-		Short: "Go-based Tool for Efficient Management",
-		Long:  "Got'tem is a simple program to manage your dotfiles",
-	}
-
-	commands := []*cobra.Command{
-    lib.ImportCmd(),
-    lib.TestCmd(),
-	}
-
-	for _, cmd := range commands {
-		rootCmd.AddCommand(cmd)
-	}
-
-	if err := rootCmd.Execute(); err != nil {
-		log.Fatal(err)
-	}
+	cmd.Execute()
 }
